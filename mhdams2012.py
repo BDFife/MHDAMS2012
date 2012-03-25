d7b63ac25dce48f9b713b7c9472d79b44589694f
Brian Fife <brian@bluesock.com>
2012-03-25 02:39:09 -0400

    Got regexp working. Want it to be fancier, but pattern matching quickly explodes in complexity.

--- mhdams2012.py
+++ mhdams2012.py
@@ -8,33 +8,62 @@ import json
 import time
 
 import twilio.twiml
+import re
 
 app = Flask (__name__)
 
 def msg_trim(msg, msg_array):
-	if len(msg) < 160:
-		msg_array.append(msg)
-	else:
-		index = msg.rfind('\r\n', 0, 160)
-		index = (index + 1)
-		msg_array.append(msg[:index])
-		msg_trim(msg[index:], msg_array)	
-	return msg_array
+    if len(msg) < 160:
+        msg_array.append(msg)
+    else:
+        index = msg.rfind('\r\n', 0, 160)
+        index += 1
+        msg_array.appebnd(msg[:index])
+        msg_trim(msg[index:], msg_array)
+    return msg_array
+
+def cmd_parse(cmd):
+    command_ref = re.compile(r"""
+        [\s]*               # allow initial whitespace
+        (?P<cmd>            # identify as the "command"
+         name|              # artist name
+         album|             # album name
+         bio|               # artist biography
+         review)            # album review
+        [\s]+               # allow interim whitespace
+        (?P<data>           # identify as the "data"
+         .+)                # (data is any remaining chars)
+                            # TBD, make non-greedy, 
+                            # trim whitespace
+    """, re.VERBOSE)
+
+    result = command_ref.match(cmd)
+    # if no match was found, result will be None
+    if result:
+        my_cmd = result.group('cmd')
+        my_data = result.group('data')
+    else:
+        my_cmd = 'help'
+        my_data = None
+
+    return my_cmd, my_data
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        resp = twilio.twiml.Response()
-        resp.sms(str("You said: " + request.form["Body"]))
-        return str(resp)
-
     if request.method == 'GET':
         resp = twilio.twiml.Response()
-        resp.sms("This starbase is fully operational")
+        resp.sms("This needs to be a POST")
+        return str(resp)
+
+    if request.method == 'POST':
+        cmd_text = request.form.get("Body")
+        cmd, data = cmd_parse(cmd_text)
+        resp = twilio.twiml.Response()
+        resp.sms("You asked for " + str(cmd )+ " of " + str(data))
         return str(resp)
 
 @app.route('/album/info/', methods=['POST', 'GET'])
-def show_album():
+def album():
     if request.method == 'GET':
         resp = twilio.twiml.Response()
         resp.sms("You need to use a POST method with this URL")
